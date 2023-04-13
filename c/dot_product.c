f199b2e70ea8e732b89c6b710c28536dfc321ad0
Alan Mikhak <amikhak@wirelessfabric.com>
2023-04-13 10:22:14 -0400

    Add dot_f32_neon()

--- c/dot_product.c
+++ c/dot_product.c
@@ -15,6 +15,23 @@
 
 static bool debug = true;
 
+#if defined(__ARM_NEON__) || defined(__ARM_NEON) || defined(__arm64__) || defined(__aarch64__)
+#include <arm_neon.h>
+float dot_f32_neon(const float* v1, const float* v2, size_t n)
+{
+    float32x4_t q1, q2;
+    float32x4_t sum = vdupq_n_f32(0.f);
+    assert(v1 && v2 && n > 0);
+    assert(n % 4 == 0);
+    do {
+        q1 = vld1q_f32(&v1[i]);
+        q2 = vld1q_f32(&v2[i]);
+        sum = vmlaq_f32(sum, q1, q2);
+    } while (n -= 4);
+    return sum;
+}
+#endif
+
 float dot_f32(const float* v1, const float* v2, size_t n)
 {
     float sum = 0.f;
@@ -29,14 +46,11 @@ float dot_f32(const float* v1, const float* v2, size_t n)
 
 int main(int argc, char *argv[])
 {
-    #define N 10
-    float zero[N] = { 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f };
-    float one[N] = { 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f };
-    float v1[N] = { 0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
-    float v2[N] = { 0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f };
-
-    printf("dot_f32: (%d) 0 . 0 = %f\n", N, dot_f32(zero, zero, N));
-    printf("dot_f32: (%d) 1 . 1 = %f\n", N, dot_f32(one, one, N));
+    #define N 16
+    float zero[N] = { 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f };
+    float one[N] = { 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f };
+    float v1[N] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f };
+    float v2[N] = { 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f };
 
     if (debug) {
         printf("v1: ");
@@ -47,6 +61,14 @@ int main(int argc, char *argv[])
     }
 
     printf("dot_f32: (%d) v1 . v2 = %f\n", N, dot_f32(v1, v2, N));
+    printf("dot_f32: (%d) 1 . 1 = %f\n", N, dot_f32(one, one, N));
+    printf("dot_f32: (%d) 0 . 0 = %f\n", N, dot_f32(zero, zero, N));
+
+#if defined(__ARM_NEON__) || defined(__ARM_NEON) || defined(__arm64__) || defined(__aarch64__)
+    printf("dot_f32_neon: (%d) v1 . v2 = %f\n", N, dot_f32_neon(v1, v2, N));
+    printf("dot_f32_neon: (%d) 1 . 1 = %f\n", N, dot_f32_neon(one, one, N));
+    printf("dot_f32_neon: (%d) 0 . 0 = %f\n", N, dot_f32_neon(zero, zero, N));
+#endif
 
     return 0;
 }
