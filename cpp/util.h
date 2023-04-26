d1d9b5a7119c9fcf3f0940f7de0fdf908d25ae3d
Alan Mikhak <amikhak@wirelessfabric.com>
2023-04-25 20:22:22 -0400

    Move various utility functions to util.h

---
+++ cpp/util.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+template <typename T>
+static void reverse(T *v, int n) {
+    assert(v && n > 0);
+    auto tail{ &v[n - 1] };
+    while (v < tail) {
+        auto f{ *v };
+        *v++ = *tail;
+        *tail-- = f;
+    }
+}
+
+template <typename T>
+float sum(const T* v, int n) {
+    assert(v && n > 0);
+    auto sum{ 0.f };
+    do sum += *v++; while (--n);
+    return sum;
+}
+
+template <typename T>
+float mean(const T* v, int n) {
+    return n ? sum(v, n) / (float)n : 0.f;
+}
+
+#endif // _UTIL_H_
