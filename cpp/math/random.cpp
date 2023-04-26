7c001d4843aaa9964d6e459eba720746cc47fd81
Alan Mikhak <amikhak@wirelessfabric.com>
2023-04-26 14:24:38 -0400

    Refactor single random value generation vs vector

--- cpp/math/random.cpp
+++ cpp/math/random.cpp
@@ -32,28 +32,30 @@
 #include "print.h"
 #include "speedup.h"
 
-// https://www.youtube.com/watch?v=N2AM6ixC6LI @ 3:23
 template <typename T>
 requires std::is_integral_v<T>
-static void random_std_mt19937(std::vector<T>& v) {
+static T random_std_mt19937(void) {
     static std::mt19937 g(std::random_device{}());
     static std::uniform_int_distribution<T> d;
-    for (auto& data : v)
-        data = d(g);
+    return d(g);
 }
 
 template <typename T>
 requires std::is_floating_point_v<T>
-static void random_std_mt19937(std::vector<T>& v) {
+static T random_std_mt19937(void) {
     static std::mt19937 g(std::random_device{}());
     static std::uniform_real_distribution<T> d;
-    for (auto& data : v)
-        data = d(g);
+    return d(g);
 }
 
-// https://www.youtube.com/watch?v=N2AM6ixC6LI @ 11:12
 template <typename T>
-static void random_dice_roll_std_rand(std::vector<T>& v) {
+static void random_std_mt19937(std::vector<T>& v) {
+    for (auto& data : v)
+        data = random_std_mt19937<T>();
+}
+
+template <typename T>
+static void dice_roll_std_rand(std::vector<T>& v) {
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     std::srand(ts.tv_nsec);
@@ -61,23 +63,26 @@ static void random_dice_roll_std_rand(std::vector<T>& v) {
         data = std::rand() % 6u + 1u;
 }
 
-// https://www.youtube.com/watch?v=N2AM6ixC6LI @ 12:42
 template <typename T>
 requires std::is_integral_v<T>
-static void random_dice_roll_std_mt19937(std::vector<T>& v) {
+static T dice_roll_std_mt19937(void) {
     static std::mt19937 g(std::random_device{}());
     static std::uniform_int_distribution<T> d((T)1, (T)6);
-    for (auto& data : v)
-        data = d(g);
+    return d(g);
 }
 
 template <typename T>
 requires std::is_floating_point_v<T>
-static void random_dice_roll_std_mt19937(std::vector<T>& v) {
+static T dice_roll_std_mt19937(void) {
     static std::mt19937 g(std::random_device{}());
     static std::uniform_real_distribution<T> d((T)1, (T)6);
+    return (T)(int)d(g);
+}
+
+template <typename T>
+static void dice_roll_std_mt19937(std::vector<T>& v) {
     for (auto& data : v)
-        data = (T)(int)d(g);
+        data = dice_roll_std_mt19937<T>();
 }
 
 template <typename T>
@@ -93,10 +98,10 @@ static void example(void (*f)(std::vector<T>&), int n) {
 
 static void f1(void) { example<int>(random_std_mt19937, 10); }
 static void f2(void) { example<float>(random_std_mt19937, 10); }
-static void f3(void) { example<int>(random_dice_roll_std_rand, 12); }
-static void f4(void) { example<float>(random_dice_roll_std_rand, 12); }
-static void f5(void) { example<int>(random_dice_roll_std_mt19937, 12); }
-static void f6(void) { example<float>(random_dice_roll_std_mt19937, 12); }
+static void f3(void) { example<int>(dice_roll_std_rand, 50); }
+static void f4(void) { example<float>(dice_roll_std_rand, 50); }
+static void f5(void) { example<int>(dice_roll_std_mt19937, 50); }
+static void f6(void) { example<float>(dice_roll_std_mt19937, 50); }
 
 static std::vector<void (*)(void)> examples {
     f1, f2, f3, f4, f5, f6
