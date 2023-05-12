de1dfb2edd9ff599021ed4d052e821aefd8048f1
Alan Mikhak <amikhak@wirelessfabric.com>
2023-05-12 11:27:05 -0400

    Update compile notes for word_count.cpp

--- cpp/string/word_count.cpp
+++ cpp/string/word_count.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 //
-// g++ -std=c++17 -O3 -lstdc++ -lm word_count.cpp -o word_count
-// g++ --version 9.3.0
+// g++ -std=c++17 -O3 word_count.cpp -o word_count
+// g++ --version 11.3.0 on soho x86_64 ubuntu 22.04
 //
 // %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 // cl /EHsc /std:c++20 /I.. word_count.cpp
